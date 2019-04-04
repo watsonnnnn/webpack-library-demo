f15c3c0e46218dd009c39f40800d2ad9397f93bf
nicewahson <nicelj92@gmail.com>
2019-04-04 14:34:20 +0800

    add externals explanation

--- webpack-library/dist/webpack-numbers1123(amd).js
+++ webpack-library/dist/webpack-numbers1123(amd).js
@@ -1,14 +1,4 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("lodash"));
-	else if(typeof define === 'function' && define.amd)
-		define(["lodash"], factory);
-	else if(typeof exports === 'object')
-		exports["webpackNumbers223111"] = factory(require("lodash"));
-	else
-		root["webpackNumbers223111"] = factory(root["_"]);
-})(window, function(__WEBPACK_EXTERNAL_MODULE_lodash__) {
-return /******/ (function(modules) { // webpackBootstrap
+define("webpackNumbers223", ["lodash"], function(__WEBPACK_EXTERNAL_MODULE_lodash__) { return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
@@ -104,7 +94,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"numToWord\", function() { return numToWord; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"wordToNum\", function() { return wordToNum; });\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"lodash\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _ref_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ref.json */ \"./src/ref.json\");\nvar _ref_json__WEBPACK_IMPORTED_MODULE_1___namespace = /*#__PURE__*/__webpack_require__.t(/*! ./ref.json */ \"./src/ref.json\", 1);\n\r\n\r\n\r\nfunction numToWord(num) {\r\n  return lodash__WEBPACK_IMPORTED_MODULE_0___default.a.reduce(_ref_json__WEBPACK_IMPORTED_MODULE_1__, (accum, ref) => {\r\n    return ref.num === num ? ref.word : accum;\r\n  }, '');\r\n}\r\n\r\nfunction wordToNum(word) {\r\n  return lodash__WEBPACK_IMPORTED_MODULE_0___default.a.reduce(_ref_json__WEBPACK_IMPORTED_MODULE_1__, (accum, ref) => {\r\n    return ref.word === word && word.toLowerCase() ? ref.num : accum;\r\n  }, -1);\r\n}\n\n//# sourceURL=webpack://webpackNumbers223111/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"numToWord\", function() { return numToWord; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"wordToNum\", function() { return wordToNum; });\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ \"lodash\");\n/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _ref_json__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ref.json */ \"./src/ref.json\");\nvar _ref_json__WEBPACK_IMPORTED_MODULE_1___namespace = /*#__PURE__*/__webpack_require__.t(/*! ./ref.json */ \"./src/ref.json\", 1);\n\r\n\r\n\r\nfunction numToWord(num) {\r\n  return lodash__WEBPACK_IMPORTED_MODULE_0___default.a.reduce(_ref_json__WEBPACK_IMPORTED_MODULE_1__, (accum, ref) => {\r\n    return ref.num === num ? ref.word : accum;\r\n  }, '');\r\n}\r\n\r\nfunction wordToNum(word) {\r\n  return lodash__WEBPACK_IMPORTED_MODULE_0___default.a.reduce(_ref_json__WEBPACK_IMPORTED_MODULE_1__, (accum, ref) => {\r\n    return ref.word === word && word.toLowerCase() ? ref.num : accum;\r\n  }, -1);\r\n}\n\n//# sourceURL=webpack://webpackNumbers223/./src/index.js?");
 
 /***/ }),
 
@@ -115,7 +105,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) *
 /*! exports provided: 0, 1, 2, 3, 4, 5, default */
 /***/ (function(module) {
 
-eval("module.exports = [{\"num\":1,\"word\":\"One\"},{\"num\":2,\"word\":\"Two\"},{\"num\":3,\"word\":\"Three\"},{\"num\":4,\"word\":\"Four\"},{\"num\":5,\"word\":\"Five\"},{\"num\":0,\"word\":\"Zero\"}];\n\n//# sourceURL=webpack://webpackNumbers223111/./src/ref.json?");
+eval("module.exports = [{\"num\":1,\"word\":\"One\"},{\"num\":2,\"word\":\"Two\"},{\"num\":3,\"word\":\"Three\"},{\"num\":4,\"word\":\"Four\"},{\"num\":5,\"word\":\"Five\"},{\"num\":0,\"word\":\"Zero\"}];\n\n//# sourceURL=webpack://webpackNumbers223/./src/ref.json?");
 
 /***/ }),
 
@@ -126,9 +116,8 @@ eval("module.exports = [{\"num\":1,\"word\":\"One\"},{\"num\":2,\"word\":\"Two\"
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("module.exports = __WEBPACK_EXTERNAL_MODULE_lodash__;\n\n//# sourceURL=webpack://webpackNumbers223111/external_%7B%22commonjs%22:%22lodash%22,%22amd%22:%22lodash%22,%22root%22:%22_%22,%22commonjs2%22:%22lodash%22%7D?");
+eval("module.exports = __WEBPACK_EXTERNAL_MODULE_lodash__;\n\n//# sourceURL=webpack://webpackNumbers223/external_%7B%22commonjs%22:%22lodash%22,%22amd%22:%22lodash%22,%22root%22:%22_%22,%22commonjs2%22:%22lodash%22%7D?");
 
 /***/ })
 
-/******/ });
-});
+/******/ })});;
--- webpack-library/webpack.config.js
+++ webpack-library/webpack.config.js
@@ -2,19 +2,23 @@ var path = require('path');
 
 module.exports = {
   entry: './src/index.js',
+  // target: 'node',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'webpack-numbers1123.js', // 打出的文件名
-    library: 'webpackNumbers223', // 在环境中的名字
-    libraryTarget: 'amd' // 打出的包所支持的环境
+    library: 'webpackNumbers223111', // 在环境中的名字
+    libraryTarget: 'umd' // 打出的包所支持的环境
   },
   externals: {
+
+    // !!! An object with { root, amd, commonjs, ... } is only allowed for libraryTarget: 'umd'. It's not allowed for other library targets.
     lodash: { // key表示的是本地代码里import xx from 'lodash'中的lodash value表示的是代码所在环境里的模块，也就是将引入模块的方式根据配置替换成指定环境下的引入和模块名
       commonjs: 'lodash',
       amd: 'lodash',
       root: '_',
       commonjs2: 'lodash'
     }
+    // lodash: '_'
   },
   mode:'development'
 };
